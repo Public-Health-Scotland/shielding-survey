d21fdb9588570681a7ff02d1d54677439ef41c39
RosalynLP <rosalyn.pearson@phs.scot>
2022-01-13 15:02:31 +0000

    update percentage function to account for multiple nans

--- shielding_functions.R
+++ shielding_functions.R
@@ -5,22 +5,22 @@
 gen.data <- function(x, wts = NA){
   
   if(!is.na(wts)){
-    out <- questionr::wtd.table(x, weights = wts)
+    out <- questionr::wtd.table(x, weights = wts, useNA = "always")
   } else {
-    out <- table(x)
+    out <- table(x, useNA = "always")
   }
   
   return(out)
 }
 
 # Function to pull necessary data
-pull_question_data <- function(keyword, qstoignore = c(), omitnans = FALSE) {
+pull_question_data <- function(keyword, qstoignore = c()) {
  
   subq <- cs %>% select(contains(keyword), RescaledWeight) %>%  
     select(-qstoignore)
-  if(omitnans){
-    subq %<>% na.omit() 
-  }
+#  if(omitnans){
+#    subq %<>% na.omit() 
+#  }
   wts <- subq$RescaledWeight
   subq %<>% select(-RescaledWeight)
   
@@ -46,21 +46,45 @@ pull_question_data <- function(keyword, qstoignore = c(), omitnans = FALSE) {
 
 
 # Function to add percentages
-add_percentages <- function(df, addna = TRUE, naans = "Not applicable"){
+add_percentages <- function(df){
+  
+  naans = c("Not applicable", "NA", "N/A", 
+            "I am not sure / Not applicable",
+            "I am not sure")   ## Possible not applicable answers
+  
+  # Define not in operator
+  `%!in%` <- Negate(`%in%`)
+  
+  # Convert all dots in dataframe to space and trim spaces
+  df$Answer <- str_replace_all(df$Answer, "\\.", " ")
+  df$Answer <- str_trim(df$Answer)
+
   newcol <- vector() # empty vector to fill
-  if(addna){
-    # Reorder not applicable to end
-    df$Answer <- factor(df$Answer, levels=c(unique(df$Answer)[unique(df$Answer) != naans], naans))
-    df <- df[order(df$Question, df$Answer),]
+  # Reorder not applicable answers to end
+  num_naan <- 0
+  for(naan in naans){
+    if(naan %in% unique(df$Answer)){
+      num_naan <- num_naan + 1
+    }
   }
+  
+  df$Answer <- factor(df$Answer, levels=c(unique(df$Answer)[unique(df$Answer) %!in% naans], naans))
+    
+  df <- df[order(df$Question, df$Answer),]
+  
   for(q in unique(df$Question)){
-    nums <- df$`Weighted`[df$Question == q & df$Answer != naans]
+    num_naan_q <- num_naan
+    
+    nums <- df$`Weighted`[df$Question == q & df$Answer %!in% naans]
     denom <- sum(nums)
     percs = 100*nums/denom
     
-    if(addna){
+    # Add NA in % column for as many nan answers as there are
+    while(num_naan_q > 0){
+      num_naan_q <- num_naan_q -1
       percs <- c(percs, NA)
     }
+
     newcol <- c(newcol, percs) # Last NA for the non applicable column
   }
   
@@ -87,9 +111,58 @@ one_var_table <- function(varname){
   output <- left_join(tab_notw, tab) 
   
   output %<>% dplyr::rename(Answer = Var1) 
-  
+
   write.csv(output, glue("Frequency tables/{varname}.csv"), row.names=FALSE)
   
 }
+
+
+# Function to check total participants is expected
+
+check_total_participants <- function(check_table, multivar=FALSE){
+  
+  expected_total <- nrow(cs)
+  
+  if(multivar == FALSE){
+    
+    sum_nw <- sum(check_table$`Non-weighted`)
+    sum_w <- sum(check_table$Weighted)
+    if(sum_nw != expected_total){
+      
+      warning(glue("Expected total {expected_total} but got non-weighted total: {sum_nw}"))
+    } 
+    if(sum_w != expected_total){
+      warning(glue("Expected total {expected_total} but got weighted total: {sum_w}"))
+    }
+    
+    
+  }else{
+    # Multi variable table needs treated column by column
+    questions <- unique(check_table$Question)
+    
+    for(question in questions){
+      sum_nw <- sum(is_count[is_count$Question == question,]$`Non-weighted`)
+      sum_w <- sum(is_count[is_count$Question == question,]$`Weighted`)
+      
+      if(sum_nw != expected_total){
+        warning(glue("Expected total {expected_total} but for Question: {question} got non-weighted total: {sum_nw}"))
+      } 
+      if(sum_w != expected_total){
+        warning(glue("Expected total {expected_total} but for Question: {question} got weighted total: {sum_w}"))
+      }
+      
+    }
+    
+    
+  }
+  
+  
+}
+
+
+
+
+
+
 
 
